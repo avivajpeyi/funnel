6d48e384a9cfc2ef14a49c5ea2fb87d7556d7efc
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2023-09-14 12:15:05 +1200

    add nlive as arg

---
+++ studies/nested_sampling_lartillot/combine_results.py
@@ -0,0 +1,36 @@
+import glob
+import numpy as np
+
+OUTDIR = "out"
+
+
+def get_combined_data(files):
+    """Combine files into a single file"""
+    data = []
+    for f in files:
+        data.append(np.loadtxt(f, skiprows=1))
+    if len(data) == 0:
+        return np.array([])
+    return np.vstack(data)
+
+
+def main():
+    data = []
+    for d in [1, 20, 100]:
+        data_di = get_combined_data(glob.glob(f"{OUTDIR}/*_d{d}_v*.dat"))
+        if data_di.shape[0] == 0:
+            continue
+        data_di = np.hstack([np.ones((data_di.shape[0], 1)) * d, data_di])
+        data.append(data_di)
+    data = np.vstack(data)
+    print(f"Saving combined data for d={d} ({data.shape[0]} samples)")
+    np.savetxt(
+        f"{OUTDIR}/nested_sampling_lnzs.dat",
+        data,
+        header="dim ns_lnz ns_lnz_err",
+        fmt=["%03d", "%.5e", "%.5e"],
+    )
+
+
+if __name__ == "__main__":
+    main()
--- studies/nested_sampling_lartillot/run_lartillot.py
+++ studies/nested_sampling_lartillot/run_lartillot.py
@@ -3,7 +3,7 @@ from funnel.fi_core import get_fi_lnz_list
 import numpy as np
 import pandas as pd
 import argparse
-from tqdm.auto import tqdm, trange
+from tqdm.auto import trange
 from contextlib import redirect_stdout
 import io
 import os
@@ -14,16 +14,6 @@ import logging
 logging.getLogger("bilby").setLevel(logging.ERROR)
 
 OUTDIR = "out"
-
-
-def suppress_output(func):
-    def wrapper(*args, **kwargs):
-        f = io.StringIO()
-        with redirect_stdout(f):  # suppress output
-            out = func(*args, **kwargs)
-        return out
-
-    return wrapper
 
 
 class LartillotLikelihood(bilby.Likelihood):
@@ -54,68 +44,23 @@ def true_lnz(v, dim):
     return (dim / 2) * (np.log(v) - np.log(1 + v))
 
 
-@suppress_output
-def nested_sampling_lnz(v, dim, label=""):
+def nested_sampling_lnz(v, dim, label="", nlive=1000):
     likelihood = LartillotLikelihood(dim, v)
     priors = get_prior(dim)
     result = bilby.run_sampler(
         likelihood=likelihood,
         priors=priors,
         sampler="dynesty",
-        nlive=50,
-        label=f"lartillot_dynesty_d{dim}_v{v}_{label}",
+        nlive=nlive,
+        label=f"d{dim}_v{v}_nlive{nlive}_{label}",
         clean=True,
         sample="rwalk",
         save=False,
+        plot=False,
+        print_method="interval-10",
+        check_point=False,
     )
     return (result.log_evidence, result.log_evidence_err)
-
-
-def parallel_tempering_lnz(v, dim):
-    likelihood = LartillotLikelihood(dim, v)
-    priors = get_prior(dim)
-    result = bilby.run_sampler(
-        likelihood=likelihood,
-        priors=priors,
-        sampler="bilby_mcmc",
-        ntemps=10,
-        label=f"lartillot_ptmc_d{dim}_v{v}",
-        clean=False,
-        proposal_cycle="default",
-        printdt=10,
-        nsamples=2000,
-    )
-    return (result.log_evidence, result.log_evidence_err)
-
-
-def __simulate_posterior(v, dim, nsamp=1000):
-    likelihood = LartillotLikelihood(dim, v)
-    priors = get_prior(dim)
-    post = []
-    scale = np.sqrt(v / (v + 1))
-    for i in range(nsamp):
-        params = {f"x{i}": np.random.normal(scale=scale) for i in range(dim)}
-        likelihood.parameters.update(params)
-        post.append(
-            dict(
-                log_likelihood=likelihood.log_likelihood(),
-                log_prior=priors.ln_prob(likelihood.parameters),
-                **params,
-            )
-        )
-    return pd.DataFrame(post)
-
-
-def fi_lnz(v, dim, nsamp=1000):
-    lnzs, r_vals, samp = get_fi_lnz_list(
-        __simulate_posterior(v, dim, nsamp=nsamp),
-        r_vals=np.linspace(0.1, 100, 100),
-        num_ref_params=1,
-        weight_samples_by_lnl=True,
-    )
-    # only keep last 90% of lnzs
-    lnzs = lnzs[:, -int(0.9 * nsamp) :]
-    return np.median(lnzs), np.std(lnzs)
 
 
 def parse_cli_args():
@@ -124,6 +69,7 @@ def parse_cli_args():
     parser.add_argument("-d", "--dim", type=int, default=1)
     parser.add_argument("-r", "--nrep", type=int, default=50)
     parser.add_argument("-s", "--seed", type=int, default=-1)
+    parser.add_argument("-n", "--nlive", type=int, default=1000)
     args = parser.parse_args()
     if args.seed < 0:
         args.seed = np.random.randint(0, 1e5)
@@ -139,7 +85,7 @@ def __runner(args, outfile, checkpoint_n_sec=3 * 3):
     pbar = trange(args.nrep)
     for i in pbar:
         l = f"_seed{args.seed}_rep{i}"
-        data.append(nested_sampling_lnz(args.v, args.dim, l))
+        data.append(nested_sampling_lnz(args.v, args.dim, l, nlive=args.nlive))
         pbar.set_postfix_str(f"LnZ: {data[-1][0]:.2f} +/- {data[-1][1]:.2f}")
         t1 = time.time()
         if t1 - t0 > checkpoint_n_sec:
